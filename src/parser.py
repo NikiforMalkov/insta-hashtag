921e4006c9800c01eca6cc8bba65a23f1f661051
Nikifor <nikiformalkov@gmail.com>
2020-02-28 15:19:55 +0600

    feat(parser): переделана логика парсинга страницы

--- src/parser.py
+++ src/parser.py
@@ -50,32 +50,29 @@ class Parser(object):
             element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, self.selector_collection["publication"]))
             )
-
             self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            index = 0
-            # self.get_publication_collection(self, i)
             publication_collection = self.get_publication_list()
-            for i in range(0, len(publication_collection)):
-                self.get_post(publication_collection[i])
-                index = i
-
-            pub_len = self.get_publication_len()
-            print("Количество публикаций - " + str(pub_len))
-            while index < self.get_publication_len():
-                print("Количество публикаций - " + str(pub_len))
-
-                self.driver.implicitly_wait(1)
-                publication_collection = self.get_publication_list()
-                print("Новое количество публикаций" + str(len(publication_collection)))
-                old_index = index
-                for i in range(old_index, len(publication_collection)):
-                    self.get_post(publication_collection[i])
-                    index = i
-
-            # for publication in publication_collection:
-            #    self.get_post(publication)
+            publication_div = publication_collection[0].find_element_by_css_selector(self.selector_collection["publicationDiv"])
+            publication_div.click()
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.selector_collection["publicationTime"]))
+            )
+            is_last = False
+            while not is_last:
+                is_last = self.get_pagination_arrow()
         finally:
             assert "No publications on the page"
+
+    def get_pagination_arrow(self):
+        result = False
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, self.selector_collection["paginationArrow"]))
+            )
+            self.get_post()
+            return True
+        finally:
+            return result
 
     def get_publication_list(self):
         publication_collection = self.driver.find_elements_by_css_selector(
@@ -83,28 +80,14 @@ class Parser(object):
         )
         return publication_collection
 
-    def get_publication_collection(self, i):
-        publication_collection = self.get_publication_list()
-        for i in range(0, len(publication_collection)):
-            self.get_post(publication_collection[i])
-        return i
-
-    def get_publication_len(self):
-        publication_collection = self.get_publication_list()
-        return len(publication_collection)
-
-    def get_post(self, publication):
-        publication_div = publication.find_element_by_css_selector(self.selector_collection["publicationDiv"])
-        publication_div.click()
-        element = WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, self.selector_collection["publicationTime"]))
-        )
-        # description = self.driver.find_element_by_css_selector(self.selector_collection["publicationDescription"])
-        link = publication.get_attribute("href")
+    def get_post(self):
+        # window.location.href
+        link = self.driver.current_url
         date = self.driver.find_element_by_css_selector(self.selector_collection["publicationTime"])
         image_tag = self.driver.find_element_by_css_selector(self.selector_collection["publicationImage"])
         url_collection = get_url_collection(image_tag.get_attribute("srcset"))
         user_login = self.driver.find_element_by_css_selector(self.selector_collection["publicationUserLogin"])
+        next_button = self.driver.find_element_by_css_selector(self.selector_collection["paginationArrow"])
 
         if self.post.one_by_link(link) is None:
             self.post.add_post(
@@ -114,14 +97,9 @@ class Parser(object):
                 link,
                 date.get_attribute("datetime")
             )
-            self.close_popup()
+            next_button.click()
             print("Add new post")
         else:
-            self.close_popup()
-            return
+            print("Skip")
+            next_button.click()
 
-    def close_popup(self):
-        close_popup_button = self.driver.find_element_by_css_selector(
-            self.selector_collection["publicationClosePopupButton"]
-        )
-        close_popup_button.click()
